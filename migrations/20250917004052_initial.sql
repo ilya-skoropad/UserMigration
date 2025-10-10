0168d528a0abd42a91f0cc3e0ded2bc748c0314b
ilya-skoropad <ilya.skoropad0@gmail.com>
2025-10-10 19:48:09 +0300

    some changes + role + indexes

--- migrations/20250917004052_initial.sql
+++ migrations/20250917004052_initial.sql
@@ -1,48 +1,54 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE SCHEMA app;
+create schema app;
 
-CREATE TABLE "app"."role" (
-    "id" INT PRIMARY KEY,
-    "name" CHAR(20)
+create table app.role (
+    id int primary key,
+    name char(20)
 );
 
-CREATE TABLE "app"."state" (
-    "id" INT PRIMARY KEY,
-    "name" CHAR(20)
+create table app.state (
+    id int primary key,
+    name char(20)
 );
 
-CREATE TABLE "app"."user" (
-    "guid"          UUID PRIMARY KEY NOT NULL DEFAULT GEN_RANDOM_UUID(),
-    "state_id"      INT NOT NULL REFERENCES "app"."state"("id"),
-    "role_id"       INT NOT NULL REFERENCES "app"."role"("id"),
-    "nickname"      VARCHAR(255) NOT NULL,
-    "login"         CHAR(25)     NOT NULL,
-    "email"         VARCHAR(255) NOT NULL,
-    "password"      CHAR(255)    NOT NULL,
-    "created_at"    TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
-    "activation_key" CHAR(20)    NULL,
-    "activated_at " TIMESTAMPTZ  NULL,
-    "last_login_at" TIMESTAMPTZ  NULL
+create table app.user (
+    guid          uuid primary key not null default gen_random_uuid(),
+    state_id      int not null references app.state(id),
+    role_id       int not null references app.role(id),
+    nickname      varchar(255) not null,
+    login         char(25)     not null,
+    email         varchar(255) not null,
+    password      char(255)    not null,
+    created_at    timestamptz  not null default now(),
+    activation_key char(30)    null,
+    activated_at  timestamptz  null,
+    updated_at    timestamptz  null,
+    last_login_at timestamptz  null
 );
 
-INSERT INTO "app"."role"("id", "name")
-VALUES (1, 'admin'),
+create unique index idx_user_login on app.user (login);
+create unique index idx_user_email on app.user (email);
+create unique index idx_user_activation_key on app.user (activation_key);
+
+insert into app.role(id, name)
+values (1, 'admin'),
        (2, 'user');
 
-INSERT INTO "app"."state"("id", "name")
-VALUES (0, 'inactive'),
+insert into app.state(id, name)
+values (0, 'inactive'),
        (1, 'active');
 
-CREATE ROLE api LOGIN;
+create role api;
 
-GRANT SELECT, INSERT, UPDATE ON "app"."role" TO api;
-GRANT SELECT, INSERT, UPDATE ON "app"."state" TO api;
-GRANT SELECT, INSERT, UPDATE ON "app"."user" TO api;
+grant usage on schema app to api;
+grant select on app.role to api;
+grant select on app.state to api;
+grant select, insert, update on app.user to api;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP SCHEMA "app" CASCADE;
-DROP ROLE api
+drop schema app cascade;
+drop role api cascade
 -- +goose StatementEnd
